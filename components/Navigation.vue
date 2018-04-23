0f85c86f30bdfa2726a2c63f8df0cac2ca5ca6c6
Sam <17letters2sam@gmail.com>
2018-04-23 16:15:10 +0530

    Add styleguide components

---
+++ components/Navigation.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+  nav
+    router-link(v-for="l in links", :key="l.url" :to="l.url", title="l.title")
+      i.material-icons(v-if="l.icon") {{ l.icon }}
+      span(v-if="l.label") {{ l.label }}
+</template>
+
+<script>
+export default {
+  props: ['links']
+}
+</script>
+
---
+++ pages/styleguide.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+  .page.styleguide
+    header
+        navigation.primary(:links="primaryNavigation")
+    nuxt-child
+</template>
+
+<script>
+import Navigation from '@/components/Navigation'
+export default {
+    data () {
+        return {
+            primaryNavigation: [
+                { title: 'Typography', icon:'text_fields', url:'styleguide/typography'},
+                { title: 'Form', icon:'check_box', url:'styleguide/form'},
+                { title: 'UI Components', icon:'widgets', url:'styleguide/UI-components'}
+            ]
+        }
+    },
+    components: {
+        Navigation
+    }
+}   
+</script>
