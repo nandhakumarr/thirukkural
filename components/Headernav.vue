c37c8d87550c9a1c311914348ad424e1178bee68
AP Keerthika <keerthi2592@gmail.com>
2018-06-05 15:30:19 +0530

    Header Component

--- components/Headernav.vue
+++ components/Headernav.vue
@@ -1,14 +1,18 @@
 <template lang="pug">
 div.header
   div.header-logo
-    img#logoimg(src="https://lh3.googleusercontent.com/-lRuIypOTaLE/WubID5P9LqI/AAAAAAAABt8/0K5GgzHMGxESWx3E4xkUq33ly5Is1rAFQCK8BGAs/s100/2018-04-30.png", alt="")
-  div#link
-    router-link.router(v-for="nav in navigations", :key="nav.url", :to="nav.url", :title="nav.title")
-      span.label {{ nav.label }}
-    div#search
-      input.box(type="search", placeholder="What are you looking for?", name="search")
-      input.search(type="submit", value="Rechercher")
+    img#logoimg(:src="logo", alt="")
+    //- div#link
+      router-link.router(v-for="nav in navigations", :key="nav.url", :to="nav.url", :title="nav.title")
+        span.label {{ nav.label }}
+  div.search
+    input(type="search", placeholder="தேடல்")
   //-iframe( width="100%" height="480" src="https://www.youtube.com/embed/A89_enDDJn0" frameborder="0" allow="autoplay encrypted-media" allowfullscreen)
+  div.social
+    img#medialogo(:src="facebook", alt="")
+    img#medialogo(:src="twitter", alt="")
+    img#medialogo(:src="instagram", alt="")
+    img#medialogo(:src="linkedIn", alt="")
 </template>
 
 <script>
@@ -16,89 +20,69 @@ export default {
   props: ['navigations'],
   data () {
     return {
+      logo: 'https://lh3.googleusercontent.com/-lRuIypOTaLE/WubID5P9LqI/AAAAAAAABt8/0K5GgzHMGxESWx3E4xkUq33ly5Is1rAFQCK8BGAs/s100/2018-04-30.png',
+      facebook: 'http://www.ssccardinals.org/assets/images/Images/Social-Media-Icons/Facebook-Icon.png',
+      twitter: 'https://cdn2.iconfinder.com/data/icons/minimalism/512/twitter.png',
+      instagram: 'http://pngimg.com/uploads/instagram/instagram_PNG3.png',
+      linkedIn: 'https://cdn3.iconfinder.com/data/icons/free-social-icons/67/linkedin_circle_color-256.png'
     }
   }
 }
 </script>
 
 <style lang="sass" scoped>
+@import 'assets/styles/config/typography'
 .header
-  background-color: transparent
-  box-shadow: none
   max-width: 1200px
   width: 100%
   margin: 20px auto 0
   padding: 0 24px
 .header .header-logo
   float: left
-  margin: 0 320px 0 15px
   #logoimg
     width: 100px
     height: 70px
-#link
-  display: block
-  justify-content: center
-  .router
-    border-left: 1px solid #ededed
-    float: left
-    font-size: 18px
-    line-height: 78px
-    padding: 0 35px
-    display: inline-block
-    color: #000
-    text-decoration: none
-    &:hover
-      background-color: #f5f5f3
-  #search
-    margin: 10px 30px
-    display: inline-block
-    position: relative
-    float: right
-    padding: 0
-    input.box
-      height: 60px
-      font-size: 15px
-      display: inline-block
-      font-weight: 50
-      border: none
-      outline: none
-      color: #555
-      padding: 3px
-      padding-right: 60px
-      width: 0px
-      position: absolute
-      top: 0
-      right: 0
-      background: none
-      z-index: 3
-      transition: width .4s cubic-bezier(0.000, 0.795, 0.000, 1.000)
-      cursor: pointer
-    input:focus
-      width: 280px
-      z-index: 1
-      border-bottom: 1px solid #BBB
-      cursor: text
-    input:focus:hover
-      border-bottom: 1px solid #BBB q
-    input.search
-      height: 60px
-      width: 63px
-      display: inline-block
-      color:red
-      float: right
-      background: url('https://cdn1.iconfinder.com/data/icons/hawcons/32/698627-icon-111-search-24.png') center center no-repeat
-      text-indent: -10000px
-      border: none
-      position: absolute
-      top: 0
-      right: 0
-      z-index: 2
-      cursor: pointer
-      opacity: 0.4
-      cursor: pointer
-      outline: none
-      transition: opacity .4s ease
-    input.search:hover
-      opacity: 0.8
+.search
+  width: 300px
+  display: flex
+  margin-top: 22px
+  margin-left: 280px
+  border-bottom: 1px solid #EEE
+  font-family: $font-family
 
+input
+  outline: none
+  &[type=search]
+  -webkit-appearance: textfield
+  -webkit-box-sizing: content-box
+  font-size: 100%
+  &::-webkit-search-decoration, &::-webkit-search-cancel-button
+    display: none
+  &[type=search]
+    background: url('https://cdn1.iconfinder.com/data/icons/hawcons/32/698627-icon-111-search-24.png') no-repeat 9px center
+    border: none
+    padding: 9px 10px 9px 50px
+    width: 125px
+    -webkit-transition: all .5s
+    -moz-transition: all .5s
+    transition: all .5s
+    &:focus
+      width: 240px
+      outline: none
+      background-color: #fff
+      border-bottom: 1px solid #EEE
+  &:-moz-placeholder, &::-webkit-input-placeholder
+    color: #999
+    border-bottom: 1px solid #EEE
+.social
+  display: flex
+  flex-direction: row
+  align-items: flex-end
+  justify-content: flex-end
+  align-content: end
+  margin: -30px
+  #medialogo
+    width: 30px
+    height: 30px
+    cursor: pointer
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,53 +1,22 @@
 <template lang="pug">
-  section.container
-    header
-      headernav.head(:navigations="Navigation")
-<<<<<<< HEAD
-=======
-      headcont
->>>>>>> c8f699b941bc0edb15ce0a98d95e112d8495b881
-    carousel
-    aside
-      h4 This is aside
+section.container
+  header
+    headernav.head(:navigations="Navigation")
 </template>
 
 
 <script>
 import Headernav from '~/components/Headernav.vue'
-import Carousel from '~/components/Carousel.vue'
-<<<<<<< HEAD
-// import Card from '~/components/Card'
-=======
-import Card from '~/components/Card.vue'
->>>>>>> c8f699b941bc0edb15ce0a98d95e112d8495b881
-
 export default {
   data () {
     return {
       Navigation: [
-<<<<<<< HEAD
-        { title: "Home", label: "Home", url: 'home' },
-        { title: "Content", label: "Content", url: 'Content'},
-        { title: "About", label: "About", url: 'About' },
-=======
-        { title: "Home", label: "Home", url: '#' },
-        { title: "Content", label: "Content", url: '#'},
-        { title: "About", label: "About", url: '#' },
-        { title: "Login", label: "Login", url: '#' },
-        { title: "Signup", label: "Signup", url: '#' }
 
->>>>>>> c8f699b941bc0edb15ce0a98d95e112d8495b881
       ]
     }
   },
 components: {
     Headernav,
-    Carousel,
-<<<<<<< HEAD
-    //- Card
-=======
-    Card
->>>>>>> c8f699b941bc0edb15ce0a98d95e112d8495b881
   }
 }
 
@@ -55,6 +24,9 @@ components: {
 
 <style lang="sass" scoped>
 @import 'assets/styles/mixins'
+@import 'assets/styles/config/typography'
+body
+  font-family: $font-family
 .container
   padding-top: 4rem
   width: 100%
