62dcc44aabd419da4da37e87033ba7f8541477eb
rama11996 <ramanathanpri@gmail.com>
2018-07-13 18:14:25 +0530

    Load kural using async

--- components/Adhigaram.vue
+++ components/Adhigaram.vue
@@ -1,15 +1,33 @@
 <template lang="pug">
 div.athigaram
-  
+  div(v-for="data in myJson")
+    h2 {{ data.Line1 }}
+    h2 {{ data.Line2 }}
+    p
+      nuxt-link(:to="kuralLink(data.Number)") Details
+
 </template>
 
 <script>
+import Thirukkural from 'static/thirukkural.json';
 export default {
-
+  data () {
+    return {
+      myJson: Thirukkural,
+     
+    }
+  },
+  methods:{
+    kuralLink (num) {
+      return '/kuraldetail/' + num
+    }
+  }
 }
 </script>
 
 <style lang="sass" scoped>
 .athigaram
-
+  margin: 1rem
+  h2
+    margin: 1rem
 </style>
---
+++ pages/mykurals/_id/index.vue
@@ -0,0 +1,40 @@
+<template>
+<section class="single-kural">
+  <h1>{{ kural.id }}</h1>
+  <h2>{{ kural.Line1 }}</h2>
+  <h2>{{ kural.Line2 }}</h2>
+</section>
+</template>
+<script>
+export default {
+   asyncData(context){
+    return new Promise((resolve, reject) =>{
+      setTimeout(() =>{
+        resolve({
+          kural: [
+           {
+              id:"1",
+              "Line1": "அகர முதல எழுத்தெல்லாம் ஆதி",
+              "Line2": "பகவன் முதற்றே உலகு.",
+              Numbers:"1"
+            },
+            {
+              id:"2",
+              "Line1": "கற்றதனால் ஆய பயனென்கொல் வாலறிவன்",
+              "Line2": "நற்றாள் தொழாஅர் எனின்.",
+              Numbers:"2"
+            },
+            {
+              id:"3",
+              "Line1": "மலர்மிசை ஏகினான் மாணடி சேர்ந்தார்",
+              "Line2": "நிலமிசை நீடுவாழ் வார்.",
+              Numbers:"3"
+            },
+          ].find(el => el.id === context.params.id )
+        })
+      }, 1500)
+    })
+  }
+}
+</script>
+
