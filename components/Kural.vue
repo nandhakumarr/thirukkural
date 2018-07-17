91063423d2b5314145726b3c396bef22fc62fce3
AP Keerthika <keerthi2592@gmail.com>
2018-07-17 11:07:54 +0530

    Add kural page

--- components/Kural.vue
+++ components/Kural.vue
@@ -1,19 +1,29 @@
 <template lang="pug">
-.kural
-  .container
+.page.kural
+  //-.container
     .athigaram
       nuxt-link(to="/athigaram") 133 அதிகாரங்கள்
     .palgal
       nuxt-link(to="/palgal") 3 பால்கள்
     .iyalgal
       nuxt-link(to="/iyalgal") 13 இயல்கள்
+  .container
+    nuxt-link(to="/kuralLink(Number)" v-for="kural in thirukkural" key="kural")
+      h3 {{ kural.Line1 }}
+      h3 {{ kural.Line2 }}
 </template>
 
 <script>
+import Thirukkural from 'static/thirukkural'
 export default {
 data () {
   return {
-
+    thirukkural: Thirukkural
+  }
+},
+methods: {
+  kuralLink (num) {
+    return '/kuraldetail' + num
   }
 }
 }
@@ -21,15 +31,14 @@ data () {
 
 <style lang="sass" scoped>
 @import 'assets/styles/thirukkural'
-.container
-  display: flex
-  justify-content: space-evenly
-  margin: 3rem
-  align-items: center
-  .athigaram, .palgal, .iyalgal
-    border: 1px solid #ccc
-    padding: $space
+.page.kural
+  margin: 0
+  padding: 0
+  .container
+    margin: 1rem
     a
+      margin: 0 1rem
       text-decoration: none
-      color: #000
+      h3
+        color: $black
 </style>
