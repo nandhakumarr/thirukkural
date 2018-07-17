7206df72cc8ed7260de1eae9305bdbb2db5a6266
AP Keerthika <keerthi2592@gmail.com>
2018-07-17 15:15:36 +0530

    Add nuxt routes id

--- pages/index.vue
+++ pages/index.vue
@@ -22,7 +22,7 @@ export default {
       ]
     }
   },
-components: {
+  components: {
     Headernav,
     //Carousel,
     Kural,
---
+++ pages/kural/_kural_num/index.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+.page
+  kural-detail
+</template>
+
+<script>
+
+import KuralDetail from '@/components/KuralDetail'
+import thirukkural from 'static/thirukkural'
+
+export default {
+  data () {
+    return {
+
+    }
+  },
+  components: {
+    KuralDetail
+  },
+}
+ </script>
