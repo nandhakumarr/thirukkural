e9c3add968bd27e7686589c2f177f13b00bdcd77
AP Keerthika <keerthi2592@gmail.com>
2018-07-19 18:48:25 +0530

    Formatted json

--- components/Adhigaram.vue
+++ components/Adhigaram.vue
@@ -1,14 +1,19 @@
 <template lang="pug">
 .athigaram
   .Chapters
-    nuxt-link(v-for="(c,i) in Chapters", key="i", to="/") {{ c }}
-
+    //- nuxt-link(v-for="(c,i) in Chapters", key="i", to="/") {{ c }}
+    pre
+      | {{ Thirukkural }}
 </template>
 <script>
-import _ from 'lodash'
+import Thirukkural from 'static/thirukkural'
+// import _ from 'lodash'
+import Pretty from 'prettify-json'
+
 export default {
   data () {
     return {
+      Thirukkural: {},
       Chapters: [
         'கடவுள் வாழ்த்து',
         'வான் சிறப்பு',
@@ -51,6 +56,18 @@ export default {
       ],
     }
   },
+  mounted() {
+    // Thirukkural.map(function(a, i) {
+    //   a['athigaram_slug'] = a.adhikaram.replace(/ /ig, '-')
+    //   a['line_1_slug'] = a.line1.replace(/ /ig, '-')
+    //   a['line_2_slug'] = a.line2.replace(/ /ig, '-')
+    //   a['id'] = i + 1
+    // })
+    // this.Thirukkural = Pretty(Thirukkural)
+    // console.log(JSON.stringify(Thirukkural));
+    console.log(Thirukkural);
+
+  }
 }
 </script>
 
--- pages/kural/_kural_num/index.vue
+++ pages/kural/_kural_num/index.vue
@@ -19,7 +19,7 @@ export default {
   },
   computed: {
     kural () {
-      return _.find(thirukkural, { Number: parseInt(this.id) })
+      return _.find(thirukkural, { id: parseInt(this.id) })
     }
   },
   components: {
