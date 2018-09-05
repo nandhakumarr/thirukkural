d55eae08433e1ed93cc03dea341118337f740df7
Nandhakumar Ramanathan <nandhakumar.r@gmail.com>
2018-09-05 18:21:55 +0530

    Add navigation for random prev next

--- components/Kural.vue
+++ components/Kural.vue
@@ -1,8 +1,13 @@
 <template lang="pug">
 .kural(v-if="kural")
+  .navigation
+    nuxt-link(:to="`/kural/${random()}`") Random
+    nuxt-link(:to="`/kural/${prev()}`") Previous
+    nuxt-link(:to="`/kural/${next()}`") Next
   .adhigaram
+    p {{ kural.id  }}
     h3 {{ kural.paal }} / {{ kural.iyal }}
-    h3 {{ kural.adhigaram }}
+    h2 {{ kural.adhigaram }}
   .section
     p {{ kural.line1 }}
     p {{ kural.line2 }}
@@ -14,6 +19,21 @@
 import Explanation from '~/components/Explanation'
 export default {
   props: ['kural'],
+  methods: {
+    random() {
+      return Math.floor((Math.random() * 1330) + 1)
+    },
+    prev() {
+      return this.kural.id === 1
+      ? 1330
+      : (this.kural.id - 1)
+    },
+    next() {
+      return this.kural.id === 1330
+      ? 1
+      : (this.kural.id + 1)
+    }
+  },
   components: {
     Explanation
   }
